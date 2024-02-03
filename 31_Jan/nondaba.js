5fdac4488293765cae3842b25f89f2084b20b16b
Ntshangase <simphiwe323i@gmail.com>
2024-02-03 19:20:49 +0200

    javascript this functions

--- 31_Jan/nondaba.js
+++ 31_Jan/nondaba.js
@@ -1,5 +1,9 @@
 
-
+// console.log function for clickMe button
 function counter () {
     console.log("hello world");
+}
+
+function display () {
+    alert("Built by Simphiwe ... Click below to discard dialogue box");
 }
