f42ae835ea26c221e9be802e177597875fa8c131
Ntshangase <simphiwe323i@gmail.com>
2024-02-05 07:54:03 +0200

    form validation

---
+++ 2_feb/main.js
@@ -0,0 +1,8 @@
+
+/**Take age of both parents/2
+ * if Age/2 < 60, gender = non-binary
+ * if Age/2 > 60 && even number, gender = male
+ * if Age2 > 60 && odd number, gender = female
+ */
+
+// This means i will need a page to redirect to to reveal the gender or an Alert/Pop-up will be fine
