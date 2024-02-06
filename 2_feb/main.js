662bbaf7935976fc68b1d9064d2f6be08f1ae965
Ntshangase <simphiwe323i@gmail.com>
2024-02-06 07:21:30 +0200

    button type submit

--- 2_feb/main.js
+++ 2_feb/main.js
@@ -1,8 +1,18 @@
+    //Take age of both parents/2
+    let x = 0;
+    // if Age/2 < 60, gender = non-binary
+    // if Age/2 > 60 && even number, gender = male
+    // Age2 > 60 && odd number, gender = female
+ 
+     
+    function Submit(){
+        
+        x = document.getElementById("motherAge").value;
+    }
 
-/**Take age of both parents/2
- * if Age/2 < 60, gender = non-binary
- * if Age/2 > 60 && even number, gender = male
- * if Age2 > 60 && odd number, gender = female
- */
-
+    function Reveal(){
+        console.log(x);
+        
+    }
+    
 // This means i will need a page to redirect to to reveal the gender or an Alert/Pop-up will be fine
