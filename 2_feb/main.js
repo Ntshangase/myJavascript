ddfe15fc99ca803ff28d9e419a6e4f6c4b35def9
Ntshangase <simphiwe323i@gmail.com>
2024-02-06 07:41:13 +0200

    console.log

--- 2_feb/main.js
+++ 2_feb/main.js
@@ -1,18 +1,24 @@
     //Take age of both parents/2
     let x = 0;
+    let y = 0;
     // if Age/2 < 60, gender = non-binary
     // if Age/2 > 60 && even number, gender = male
     // Age2 > 60 && odd number, gender = female
  
      
     function Submit(){
-        
+        // load the input.values to a variable for manipulation later
         x = document.getElementById("motherAge").value;
+        y = document.getElementById("fatherAge").value;
+
+        // clear the input fields
         document.getElementById("motherAge").value= '';
+        document.getElementById("fatherAge").value= '';
     }
 
     function Reveal(){
         console.log(x);
+        console.log(y);
         
     }
     
