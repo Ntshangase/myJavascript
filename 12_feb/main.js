41b366afd451a06e78682ff117b45083c4d0dad1
Ntshangase <simphiwe323i@gmail.com>
2024-02-17 10:55:15 +0200

    debugg expert

--- 12_feb/main.js
+++ 12_feb/main.js
@@ -1,14 +1,13 @@
 
 // changecolor function
 function changeColor(){
-    // to change color in Javascript.
-    document.getElementById("body").style.color = "yellow";
 
     //now i need to have a math.random which continually cahnges when click.
         // let try with 3 self-defined colors first...2.it will be easier if i 
         // create an array with all the colors
     let array = ["green", "brown", "pink", "blue"];
 
-    // random number to be passed in array.
-    console.log(Math.floor(Math.random()*array.length));
+    // random number to be passed in array.// to change color in Javascript.
+    document.getElementById("body").style.backgroundColor = `${array[Math.floor(Math.random()*array.length)]}`;
+
 }
