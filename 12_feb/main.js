5ebd8e1d3303c4a4af82d7220fd5e405ee47056e
Ntshangase <simphiwe323i@gmail.com>
2024-02-18 16:07:05 +0200

    X/O active

--- 12_feb/main.js
+++ 12_feb/main.js
@@ -1,34 +1,33 @@
-
 // changecolor function
-function changeColor(){
+function changeColor() {
+	//now i need to have a math.random which continually cahnges when click.
+	// let try with 3 self-defined colors first...2.it will be easier if i
+	// create an array with all the colors
+	let array = ["green", "brown", "pink", "blue"];
 
-    //now i need to have a math.random which continually cahnges when click.
-        // let try with 3 self-defined colors first...2.it will be easier if i 
-        // create an array with all the colors
-    let array = ["green", "brown", "pink", "blue"];
-
-    // random number to be passed in array.// to change color in Javascript.
-    document.getElementById("body").style.backgroundColor = `${array[Math.floor(Math.random()*array.length)]}`;
-
+	// random number to be passed in array.// to change color in Javascript.
+	document.getElementById("body").style.backgroundColor = `${
+		array[Math.floor(Math.random() * array.length)]
+	}`;
 }
 
-// TIC TAC TOE 
+// TIC TAC TOE
 
 // take user input and place it.
 
-
 // inster symbol function
 function insert() {
-    document.getElementById("C").innerHTML = "X";
+    let xo = document.getElementById("select1").value
+	document.getElementById("C").innerHTML = `${xo}`;
+    
 }
-
 
 // restart game
 function restart() {
-    let boxes = document.getElementById("table").getElementsByTagName("td");
+	let boxes = document.getElementById("table").getElementsByTagName("td");
 
-    // a loop to iterate through returned collection and set innerHTML = "";
-    for (let count=0; count<boxes.length; count++){
-        boxes[count].innerHTML = "";
-    }
+	// a loop to iterate through returned collection and set innerHTML = "";
+	for (let count = 0; count < boxes.length; count++) {
+		boxes[count].innerHTML = "";
+	}
 }
