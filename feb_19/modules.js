f15412bf21e5774887d630d6ed056c399fcf6000
Ntshangase <simphiwe323i@gmail.com>
2024-02-25 20:52:39 +0200

    Impumelelo Igugu

--- feb_19/modules.js
+++ feb_19/modules.js
@@ -1,8 +1,12 @@
-
 /**A javascript file cannot communicate between 2 Dom at the same time. */
 
 // There can only be oe Dom.
-export function displayModule () {
-
-    console.log('back');
+export function displayModule() {
+	let textDiv = document.querySelector(".to_nav");
+	textDiv.innerHTML = `<div class="nav"> 
+    <a href="index.html" class="nav-item">Home</a>
+    <a href="About.html" class="nav-item">About</a>
+    <a href="Contact.html" class="nav-item">Contact</a>
+    </div>`;
 }
+
