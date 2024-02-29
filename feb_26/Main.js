6a8e005eb795f126ac5aff50b5b109f8dddfef0b
Ntshangase <simphiwe323i@gmail.com>
2024-02-29 19:54:12 +0200

    I really don't like this commit as it contains horrible code. The only good thing is that it works.

--- feb_26/Main.js
+++ feb_26/Main.js
@@ -1,9 +1,24 @@
 
 
 // addTOCartFunctionality
+// Below is one of the most horrible code i have written in my entire life.
 
 function addToCart() {
     let itemValue = document.getElementById("select").value;
+    itemValue = Number(itemValue)
+    console.log(itemValue);
+    console.log("proof:  ", itemValue+2)
+}
+
+function addToCart2() {
+    let itemValue = document.getElementById("select2").value;
+    itemValue = Number(itemValue)
+    console.log(itemValue);
+    console.log("proof:  ", itemValue+2)
+}
+
+function addToCart3() {
+    let itemValue = document.getElementById("select3").value;
     itemValue = Number(itemValue)
     console.log(itemValue);
     console.log("proof:  ", itemValue+2)
