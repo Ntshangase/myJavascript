898a0b75a0f2fc65bb434ba7189a49d0c75969c3
Ntshangase <simphiwe323i@gmail.com>
2024-03-03 21:21:43 +0200

    linked list basics

--- The Last Algorithem Course You Will Need./linkedList.js
+++ The Last Algorithem Course You Will Need./linkedList.js
@@ -1,5 +1,5 @@
 
-// node --experimental-modules sort.js
+// node --experimental-modules linkedList.js
 
 /**linkedList are data structures similar to arrays but also fundamentally different,
  * they don't have the array strength of having an index, thus i can't iterate through an 
@@ -9,10 +9,61 @@
  * when we know we have reached the end of our linkedList.
  */
 
+/**
+ * const nodeList = { 
+ *    head :{
+ *            value= "name";
+ *             next :{
+ *                    value : "name2";
+ *                     next :{ 
+ *                            value : "name3";
+ *                             next : null
+ *          }
+ *        }
+ *     }
+ *  }
+ * }
+ */
+
 // I have to create my own node in javascript.
 class NewNode{
     constructor(data){
         this.value = data;
-        this.next = null;
+        this.next = null;   // pointer.
     }
 }
+// I have to create my own linkedList Data Structure which is just an object on objects, on objects.
+class List {
+    constructor(data = null){
+        this.head = data
+    }
+} 
+
+
+function addNode(){
+    // create a new node from node constructor.
+    let node1 = new NewNode("simphiwe");
+    let node2 = new NewNode("ntokozo");
+    let node3 = new NewNode("ntshangase");
+    let node4 = new NewNode("manxanga");
+    let node5 = new NewNode("ntshumo");
+
+    // append the nodes to the list.
+    let list1 = new List(node1);
+
+    // set node pointers.
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    node4.next = node5;  // the last node is automaticall at null.
+    
+    // consoler...
+    console.log(node1);
+    console.log(node2);
+    console.log(node3);
+    console.log(node4);
+    console.log(node5);
+}
+
+// function to run.
+addNode();
