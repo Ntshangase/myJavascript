b80a203b392763195fa74e780046aa743cd866dc
Ntshangase <simphiwe323i@gmail.com>
2024-03-10 09:20:03 +0200

    basic design

--- all-In/main.js
+++ all-In/main.js
@@ -1,8 +1,11 @@
 
 
 //function to output random numbers from 1-3.
-function numberGenarator(){
+function shakesHand(side){
     const abc = ["Rock","Paper","Scissors"]
     let x = Math.floor(Math.random() * 3);
+    //console.log(side); side arrives here correct
+    //side = "left"
+    document.getElementById("left").innerHTML = `${abc[x]}`
     console.log(abc[x]);
 }
