996292ee6d2c59e33041acc589d3bd04f5322602
Ntshangase <simphiwe323i@gmail.com>
2024-03-10 08:35:32 +0200

    Rock Paper Scissors

---
+++ all-In/main.js
@@ -0,0 +1,8 @@
+
+
+//function to output random numbers from 1-3.
+function numberGenarator(){
+    const abc = ["Rock","Paper","Scissors"]
+    let x = Math.floor(Math.random() * 3);
+    console.log(abc[x]);
+}
