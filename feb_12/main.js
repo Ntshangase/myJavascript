332fab9e73d4e99123d1c1b1f9df5c10967e2861
Ntshangase <simphiwe323i@gmail.com>
2024-03-14 01:11:23 +0200

    dark theme

--- feb_12/main.js
+++ feb_12/main.js
@@ -3,7 +3,7 @@ function changeColor() {
 	//now i need to have a math.random which continually cahnges when click.
 	// let try with 3 self-defined colors first...2.it will be easier if i
 	// create an array with all the colors
-	let array = ["green", "brown", "pink", "blue"];
+	let array = ["white", "#121212"];
 
 	// random number to be passed in array.// to change color in Javascript.
 	document.getElementById("body").style.backgroundColor = `${
